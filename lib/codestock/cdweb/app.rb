b4df3e3c02831e1888a7bc8239b94dddc1d4f73c
ongaeshi <ongaeshi0621@gmail.com>
2011-07-04 01:53:52 +0900

    Add view.haml.

--- lib/codestock/cdweb/app.rb
+++ lib/codestock/cdweb/app.rb
@@ -29,50 +29,21 @@ get '/' do
   haml :index
 end
 
-get '/::help' do
+# post '/::search' do
+# end
+
+# get '/::search' do
+#   haml :search
+# end
+
+get %r{/::view} do
+  # @title = path.sub(/^view/, "")
+  #   v = Viewer.new(request.path_info)
+  #   @record = v.record
+  haml :view
+end
+
+get %r{/::help} do
   haml :help
 end
 
-get '/*.css' do |path|
-  scss path.to_sym
-end
-
-get '/*.html' do |path|
-  pass unless File.exist?(File.join(options.views, "#{path}.haml"))
-  haml path.to_sym
-end
-
-get '/*' do |path|
-  pass unless File.exist?(File.join(options.views, "#{path}.haml"))
-  haml path.to_sym
-end
-
-# -- Sample app (delete OK) -- 
-
-get '/hello*' do |path|
-  num = path.to_i
-  num = 1 if num == 0
-
-  hello = 'Hello '
-  hello = 'Hel ' if (num > 10000)
-  hello = 'H ' if (num > 100000)
-  return "The end." if (num > 1000000)
-    
-  str = "hello#{num} -> <a href=\"hello#{num*2}\">#{num*2}</a>"
-  str += "<p>" + "#{hello}" * num + "</p>"
-  str
-end
-
-get '/weekday' do
-  wday = Time.new.wday
-  r = []
-  r << "en : #{['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][wday]}"
-  r << "ja : #{['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'][wday]}"
-  r << "fr : #{['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'][wday]}"
-  r.join("<br>")
-end
-
-# ユーザエージェントの表示
-get '/user_agent' do
-  request.user_agent
-end
