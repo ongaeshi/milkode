7cec1fd633660fbead3931beba6809fc7e7eccbf
ongaeshi <ongaeshi0621@gmail.com>
2011-12-19 00:51:11 +0900

    Add : t_not_package_root.

--- test/test_cli_grep.rb
+++ test/test_cli_grep.rb
@@ -7,6 +7,7 @@
 
 require 'milkode/grep/cli_grep.rb'
 require 'test_helper'
+require 'milkode_test_work'
 
 class TestCLI_Grep < Test::Unit::TestCase
   def setup
@@ -15,24 +16,41 @@ class TestCLI_Grep < Test::Unit::TestCase
     @work.add_package "db1", @work.expand_path("../data/b_project")
   end
 
-  def test_basic
+  def test_main
+    # 全てを test_* にすると、毎回setup, teardown が走ってデータベースを生成する時間がもったいないので
+    t_basic
+    t_not_found_package
+    t_not_package_root
+  end
+
+  def teardown
+    @work.teardown
+  end
+
+  private
+
+  def t_basic
     io = StringIO.new
 
     CLI_Grep.execute(io, "".split)
-    CLI_Grep.execute(io, "test".split)
+    CLI_Grep.execute(io, "-a test".split)
     CLI_Grep.execute(io, "-p a_project test".split)
 
-    # puts io.string
+    # io.puts
   end
 
-  def test_not_found_package
+  def t_not_found_package
     io = StringIO.new
     CLI_Grep.execute(io, "-p c_project test".split)
     assert_match "fatal:", io.string
   end
 
-  def teardown
-    @work.teardown
+  def t_not_package_root
+    Dir.chdir(File.join(File.dirname(__FILE__))) do
+      io = StringIO.new
+      CLI_Grep.execute(io, "require".split)
+      assert_match "fatal:", io.string
+    end
   end
 end
 
