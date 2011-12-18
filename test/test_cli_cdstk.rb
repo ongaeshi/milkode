dca8248ae9c6e37d358ab2c270b5dca5f34a10ee
ongaeshi <ongaeshi0621@gmail.com>
2011-12-19 00:50:47 +0900

    テストの高速化

--- test/test_cli_cdstk.rb
+++ test/test_cli_cdstk.rb
@@ -9,6 +9,7 @@ require 'milkode/cdstk/cli_cdstk.rb'
 require 'milkode/common/dbdir'
 require 'test_helper'
 require 'milkode/cdstk/cdstk_command'
+require 'milkode_test_work'
 
 class TestCLI_Cdstk < Test::Unit::TestCase
   def setup
@@ -16,13 +17,24 @@ class TestCLI_Cdstk < Test::Unit::TestCase
     @work = MilkodeTestWork.new({:default_db => true})
   end
 
-  def test_mcd
+  def test_main
+    t_mcd
+    t_setdb
+  end
+
+  def teardown
+    @work.teardown
+  end
+
+  private
+
+  def t_mcd
     io = StringIO.new
     CLI_Cdstk.execute(io, "mcd".split)
     assert_match /mcd/, io.string
   end
   
-  def test_setdb
+  def t_setdb
     # 引数無しで現在の値を表示
     io = StringIO.new
     CLI_Cdstk.execute(io, "setdb".split)
@@ -51,9 +63,5 @@ class TestCLI_Cdstk < Test::Unit::TestCase
     assert_not_equal @first_default_dir, Dbdir.default_dir
     CLI_Cdstk.execute(io, "setdb --reset".split)
     assert_equal @first_default_dir, Dbdir.default_dir
-  end
-
-  def teardown
-    @work.teardown
   end
 end
