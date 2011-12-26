0d34cf25670623294d005a3bb1f3fa2cbf959bb0
ongaeshi <ongaeshi0621@gmail.com>
2011-12-27 02:48:35 +0900

    cdv, cdview を削除

--- test/test_bin_exec.rb
+++ test/test_bin_exec.rb
@@ -8,16 +8,14 @@
 require 'rubygems'
 require 'test/unit'
 require 'milkode/cdstk/cli_cdstk'
-require 'milkode/cdview/cli_cdview'
 
 class TestBinExec < Test::Unit::TestCase
   def test_bin_exec
     io = StringIO.new
-
+    
+    # milk
     Milkode::CLI_Cdstk.execute(io)
     
-    Milkode::CLI_Cdview.execute(io)
-
     # 一定時間だけ起動するような仕組みは無いものか
     # Milkode::CLI_Cdweb.execute(io, "--no-browser -p 5555".split)
     
