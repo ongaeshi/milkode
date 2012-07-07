cc1df8d2026c0f3b1ea89ac8ab6d4f70a0b3e26b
ongaeshi <ongaeshi0621@gmail.com>
2012-07-07 21:04:00 +0900

    [cdweb] Add CodeRay options[:onclick_copy_line_number]

--- lib/milkode/cdweb/lib/coderay_wrapper.rb
+++ lib/milkode/cdweb/lib/coderay_wrapper.rb
@@ -55,7 +55,8 @@ module Milkode
               :css => :class,
               :highlight_lines => @highlight_lines,
               :line_number_start => @line_number_start,
-              :line_number_anchors => false
+              :line_number_anchors => false,
+              :onclick_copy_line_number => true
               )
     end
 
