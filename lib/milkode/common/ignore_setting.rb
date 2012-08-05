013368e32f0d08d1e4dd18a2b6055c4675195d9b
ongaeshi <ongaeshi0621@gmail.com>
2012-08-05 19:41:19 +0900

    [milk] バグ修正 : IgnoreSettingの除外設定、複雑なパターン
    is = IgnoreSetting.new "/", ["/*", "!/path/to/dir"]
    
    assert_equal false,  is.ignore?("/path")        # 親ディクレクリも除外しない
    assert_equal false,  is.ignore?("/path/to")
    assert_equal true ,  is.ignore?("/path/to/a")   # こっちは除外
    assert_equal false,  is.ignore?("/path/to/dir")

--- lib/milkode/common/ignore_setting.rb
+++ lib/milkode/common/ignore_setting.rb
@@ -5,6 +5,8 @@
 # @author ongaeshi
 # @date   2012/03/02
 
+require 'pathname'
+
 module Milkode
   class IgnoreSetting
     attr_reader :path
@@ -35,15 +37,25 @@ module Milkode
         end
       end
 
-      @regexps = @ignores.map do |v|
-        v = "(\/|\\A)" + Regexp.escape(v).gsub('\\*', "[^/]*") + "(\/|\\Z)"
-        Regexp.new(v)
+      # 除外設定 -> 正規表現
+      @regexps = @ignores.map {|v| convert_regexp(v)}
+
+      # 非除外設定 -> 展開 -> 正規表現
+      new_not_ignores = []
+      @not_regexps = []
+      
+      @not_ignores.each do |v|
+        last_regexp = "(\/|\\Z)" # 自分自身は後方マッチ可
+
+        Pathname.new(v).ascend do |pathname|
+          txt = pathname.to_s
+          new_not_ignores << txt
+          @not_regexps << convert_regexp(txt, last_regexp)
+          last_regexp = "\\Z"   # 親ディクレクリは後方マッチ不可
+        end
       end
 
-      @not_regexps = @not_ignores.map do |v|
-        v = "(\/|\\A)" + Regexp.escape(v).gsub('\\*', "[^/]*") + "(\/|\\Z)"
-        Regexp.new(v)
-      end
+      @not_ignores = new_not_ignores
     end
 
     def ignore?(path)
@@ -78,6 +90,11 @@ module Milkode
       end
       
       return false
+    end
+
+    def convert_regexp(txt, last_regexp = "(\/|\\Z)")
+      v = "(\/|\\A)" + Regexp.escape(txt).gsub('\\*', "[^/]*") + last_regexp
+      Regexp.new(v)
     end
   end
 end
