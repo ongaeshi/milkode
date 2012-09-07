7d0529c850723381c2efbe681358c373bedd7469
ongaeshi <ongaeshi0621@gmail.com>
2012-09-08 02:03:30 +0900

    Add windows.confirm

--- lib/milkode/cdweb/app.rb
+++ lib/milkode/cdweb/app.rb
@@ -162,7 +162,7 @@ EOF
     #{headicon('go-home-5.png')} <a href="/home" class="headmenu">ホーム</a>
     #{headicon('document-new-4.png')} <a href="#{href}" class="headmenu" onclick="window.open('#{href}'); return false;">新しい検索</a>
     #{headicon('directory.png')} <a href="#{flist}" class="headmenu">ディレクトリ</a> 
-    #{headicon('view-refresh-4.png')} <a href="#" class="headmenu">パッケージを更新</a>
+    #{headicon('view-refresh-4.png')} <a href="#" class="headmenu" onclick="update_package('#{path}')">パッケージを更新</a>
     #{headicon('help.png')} <a href="/help" class="headmenu">ヘルプ</a>
 EOF
   end
