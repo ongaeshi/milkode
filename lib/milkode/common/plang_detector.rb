02ff43164fa73f9688801c03ee9e986e54e4be12
ongaeshi <ongaeshi0621@gmail.com>
2012-09-30 17:56:22 +0900

    plang_detector: Add LANGUAGES

--- lib/milkode/common/plang_detector.rb
+++ lib/milkode/common/plang_detector.rb
@@ -9,15 +9,34 @@ module Milkode
   class PlangDetector
     LANGUAGES =
       [
-       { :name => 'ActionScript', :suffixs => ['as']         },
-       { :name => 'C',            :suffixs => ['c', 'h']     },
-       { :name => 'CSS',          :suffixs => ['css']        },
-       { :name => 'C#',           :suffixs => ['cs']         },
-       { :name => 'C++',          :suffixs => ['cpp', 'hpp'] },
-       { :name => 'Java',         :suffixs => ['java']       },
-       { :name => 'JavaScript',   :suffixs => ['js']         },
-       { :name => 'Ruby',         :suffixs => ['rb']         },
-       { :name => 'Text',         :suffixs => ['txt']        },
+       { :name => 'ActionScript', :suffixs => ['as']           },
+       { :name => 'C'           , :suffixs => ['c', 'h']       },
+       { :name => 'CSS'         , :suffixs => ['css']          },
+       { :name => 'C#'          , :suffixs => ['cs']           },
+       { :name => 'C++'         , :suffixs => ['cpp', 'hpp']   },
+       { :name => 'Java'        , :suffixs => ['java']         },
+       { :name => 'JavaScript'  , :suffixs => ['js']           },
+       { :name => 'Ruby'        , :suffixs => ['rb']           },
+       { :name => 'Text'        , :suffixs => ['txt']          },
+                                                                
+       { :name => 'Common Lisp' , :suffixs => ['cl']           },
+       { :name => 'Diff'        , :suffixs => ['diff']         },
+       { :name => 'Emacs Lisp'  , :suffixs => ['el']           },
+       { :name => 'Erlang'      , :suffixs => ['erl']          },
+       { :name => 'Haskell'     , :suffixs => ['hs']           },
+       { :name => 'HTML'        , :suffixs => ['html']         },
+       { :name => 'Lua'         , :suffixs => ['lua']          },
+       { :name => 'Objective-C' , :suffixs => ['m', 'mm']      },
+       { :name => 'Perl'        , :suffixs => ['pl']           },
+       { :name => 'PHP'         , :suffixs => ['php']          },
+       { :name => 'Python'      , :suffixs => ['py']           },
+       { :name => 'Scala'       , :suffixs => ['scm']          },
+       { :name => 'Yaml'        , :suffixs => ['yml', 'yaml']  },
+
+       # { :name => ''     ,  :suffixs => ['']       },
+
+
+
       ]
 
    ETC_LANGUAGE = {:name => 'etc'}
