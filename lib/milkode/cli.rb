1e55f2dd0fb3d56637786acc82130754c08d79f8
ongaeshi <ongaeshi0621@gmail.com>
2012-10-28 16:20:36 +0900

    cdstk.rb: milk remove 改善
    1. 相対パス指定を可能に : milk remove .
    2. 引数無しの時はヘルプを表示

--- lib/milkode/cli.rb
+++ lib/milkode/cli.rb
@@ -52,11 +52,15 @@ EOF
     end
 
     desc "remove keyword_or_path1 [keyword_or_path2 ...]", "Remove package"
-    option :all, :type => :boolean, :desc => 'Remove all.'
+    option :all, :type => :boolean, :aliases => '-a', :desc => 'Remove all.'
     option :force, :type => :boolean, :aliases => '-f', :desc => 'Force remove.'
     option :verbose, :type => :boolean, :aliases => '-v', :desc => 'Be verbose.'
     def remove(*args)
-      cdstk.remove(args, options)
+      if args.empty? && !options[:all]
+        CLI.task_help(shell, "remove")
+      else
+        cdstk.remove(args, options)
+      end
     end
 
     desc "list [package1 package2 ...]", "List package"
