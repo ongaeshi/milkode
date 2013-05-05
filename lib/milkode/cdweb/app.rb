75c9e39a624d3b9c54f4250e12113883b4abc235
ongaeshi <ongaeshi0621@gmail.com>
2013-05-05 19:16:26 +0900

    1.0.0.rc.1 -> 0.9.9.9
    RubyGems.org の挙動が上手くいかなかった

--- lib/milkode/cdweb/app.rb
+++ lib/milkode/cdweb/app.rb
@@ -29,7 +29,7 @@ set :haml, :format => :html5
 get '/' do
   if Database.validate?
     @setting = WebSetting.new
-    @version = "1.0.0.rc.1"
+    @version = "0.9.9.9"
 
     @package_num = Database.instance.yaml_package_num
     @file_num = Database.instance.totalRecords
